caacd9b92aa8b60978d0dc34a32d4a26663ae2f6
KD8BXP <kd8bxp@aol.com>
2017-03-01 13:22:52 -0500

    slow speed 90 degree, almost turn test code

---
+++ leftright90degreeturns/leftright90degreeturns.ino
@@ -0,0 +1,56 @@
+//Copywrite 2017 LeRoy Miller.
+//Based on time, and needs to be adjusted for different motors
+//and different surface materials.
+
+#include "BittyBot2.h"
+
+int leftspeed = 75;
+int rightspeed = 75;
+int turnDelay = 1250;
+
+BittyBot bot(44,46,36,38,40,42); //Left Enable, Right Enable, Pin1 for Left, Pin2 for Left, Pin 1 for Right, Pin 2 for Right
+
+void setup () {
+	Serial.begin(9600);
+	bot.begin();
+	
+	Serial.println("Hello!");
+}
+
+void loop() {
+  
+	turnleft();
+	Serial.println("Left!");
+
+delay(1000);
+
+Serial.println("Right!");
+  turnright();
+
+ Serial.println("End.");
+
+ while(1) {}
+}
+
+void turnleft() {
+  //turn left about 90 degrees
+  bot.Speed(leftspeed,rightspeed);
+  bot.leftTight(turnDelay); //360
+  while (bot.IsRunning()) {
+    
+    Serial.println(bot.IsRunning());
+    bot.update();
+      }
+}
+
+void turnright() {
+  //turn right about 90 degrees
+  bot.Speed(leftspeed, rightspeed);
+  bot.rightTight(turnDelay); //360
+  while (bot.IsRunning()) {
+    
+    Serial.println(bot.IsRunning());
+    bot.update();
+      }
+}
+
