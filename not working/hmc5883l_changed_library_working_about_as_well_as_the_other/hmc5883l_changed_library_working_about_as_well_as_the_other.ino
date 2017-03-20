59729a0436d56b9b75fa057ef058058dad5c2cd5
KD8BXP <kd8bxp@aol.com>
2017-03-20 19:49:24 -0400

    modified hmc5883 code, added a slightly different version of the code to not working

--- not working/hmc5883l_changed_library_working_about_as_well_as_the_other/hmc5883l_changed_library_working_about_as_well_as_the_other.ino
+++ not working/hmc5883l_changed_library_working_about_as_well_as_the_other/hmc5883l_changed_library_working_about_as_well_as_the_other.ino
@@ -1,7 +1,8 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <HMC5883L_Simple.h>
+#include "I2Cdev.h"
+#include "HMC5883L.h"
 #include "BittyBot2.h"
 
 int leftspeed = 175;
@@ -11,16 +12,18 @@ float startHeading;
 float heading;
 
 BittyBot bot(44,46,36,38,40,42); //Left Enable, Right Enable, Pin1 for Left, Pin2 for Left, Pin 1 for Right, Pin 2 for Right
-HMC5883L_Simple Compass;
+HMC5883L Compass;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
   bot.begin();
-  Compass.SetDeclination(-7, 53, 'W');
+  Compass.initialize();
+  /*Compass.SetDeclination(-7, 53, 'W');
   Compass.SetSamplingMode(COMPASS_CONTINUOUS);
   Compass.SetScale(COMPASS_SCALE_190);
   Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
+  */
   delay(5000); //wait for robot to be placed on table
 }
 
@@ -41,12 +44,17 @@ delay(1000);
 }
 
 void turn(int angle) {
-  int originalAngle=Compass.GetHeadingDegrees();
-  originalAngle=originalAngle%360;   //added by me
-  if (originalAngle<0) {originalAngle+=360;} //added by me
+  int16_t mx, my, mz;
+  Compass.getHeading(&mx, &my, &mz);
+  //int originalAngle=Compass.GetHeadingDegrees();
+  float originalAngle = atan2(my, mx);
+  if (originalAngle<0) {originalAngle += 2*M_PI;} 
+  originalAngle = originalAngle * 180/M_PI;
+  //originalAngle = map(originalAngle, 0, 360, 246, 360);
   Serial.print("Str: ");
   Serial.println(originalAngle);
   int target=originalAngle+angle;
+  //target= map(target, target, 360, 0, 360);
   //Serial.print("Trg: ");
   //Serial.println(target);
   //target=angle;
@@ -59,8 +67,13 @@ void turn(int angle) {
   }
   int direction=angle;
   while(1){
-    int currentAngle=Compass.GetHeadingDegrees();
-    
+    //int currentAngle=Compass.GetHeadingDegrees();
+    int16_t mx, my, mz;
+  Compass.getHeading(&mx, &my, &mz);
+  float currentAngle = atan2(my, mx);
+  if (currentAngle<0) {currentAngle += 2*M_PI;} 
+  currentAngle = currentAngle * 180/M_PI;
+  //currentAngle = map(currentAngle, 0, 360, 246, 360);
     int diff=target-currentAngle;
     direction=180 - (diff+360)%360;
     
@@ -83,14 +96,14 @@ void turn(int angle) {
       }
           }
     
-    if(abs(diff)<60 ) {
+    if(abs(diff)<60) {
       bot.stop();
       return;
     }
-    if (abs(diff)>114) {
-      bot.stop();
-      return;
-    }
+    //if (abs(diff)>100 && direction<0) {
+     // bot.stop();
+     // return;
+    //}
   }
 }
 
