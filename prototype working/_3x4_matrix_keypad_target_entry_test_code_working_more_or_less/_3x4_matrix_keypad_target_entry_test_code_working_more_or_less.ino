43c7139e287e7c393dd66ee30762dd7ca13e9d25
KD8BXP <kd8bxp@aol.com>
2017-03-21 22:13:14 -0400

    updated 3x4 matrix keypad for mega

--- prototype working/_3x4_matrix_keypad_target_entry_test_code_working_more_or_less/_3x4_matrix_keypad_target_entry_test_code_working_more_or_less.ino
+++ prototype working/_3x4_matrix_keypad_target_entry_test_code_working_more_or_less/_3x4_matrix_keypad_target_entry_test_code_working_more_or_less.ino
@@ -9,8 +9,11 @@ char keys[keyROWS][keyCOLS] = {
 {'*','0','#'}
 };
 
-byte rowPins[keyROWS] = {8, 7, 6, 5}; // row pinouts of the keypad R1 = D8, R2 = D7, R3 = D6, R4 = D5
-byte colPins[keyCOLS] = {4, 3, 2};    // column pinouts of the keypad C1 = D4, C2 = D3, C3 = D2
+byte rowPins[keyROWS] = {49,47,45,43};
+byte colPins[keyCOLS] = {41,39,37};
+
+//byte rowPins[keyROWS] = {8, 7, 6, 5}; // row pinouts of the keypad R1 = D8, R2 = D7, R3 = D6, R4 = D5
+//byte colPins[keyCOLS] = {4, 3, 2};    // column pinouts of the keypad C1 = D4, C2 = D3, C3 = D2
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, keyROWS, keyCOLS);
 
 
@@ -24,25 +27,31 @@ int COLS = 5;
 void setup()
 {
   Serial.begin(9600);
+  Serial.write(17);
 }
  
 void loop()
 {
  while (targetrow < 0 || targetrow > ROWS) {
+  Serial.write(12);
   Serial.print("Row? ");
   targetrow = inputTarget();
   if (targetrow > ROWS) {Serial.println("Out of Bounds");}
  }
  while (targetcol < 0 || targetcol > COLS) {
+  Serial.write(12);
   Serial.print("Col? ");
   targetcol = inputTarget();
   if (targetcol > COLS) {Serial.println("Out of Bounds");}
  }
+ Serial.write(12);
   Serial.print("Target: ");
   Serial.print(targetrow);
   Serial.print(",");
   Serial.println(targetcol);
   delay(1000);
+  targetrow = -999;
+  targetcol = -999;
 }
 
 
